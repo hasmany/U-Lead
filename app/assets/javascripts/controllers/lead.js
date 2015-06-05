eb7cb896b256f4f6aebbfdd6660c5c1b895267ff
Hubert Nguyen <hubertanguyen@gmail.com>
2015-06-05 13:47:47 -0500

    add functionality to delete a lead

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -7,6 +7,12 @@ App.LeadController = Ember.ObjectController.extend({
     saveChanges: function() {
       this.get('model').save();
     },
+    delete: function(){
+      var self = this;
+      this.get('model').destroyRecord().then(function(){
+        self.transitionToRoute('leads');
+      });
+    }
   },
   showUnsavedMessage: function() {
     return this.get('isDirty') && !this.get('isSaving');
