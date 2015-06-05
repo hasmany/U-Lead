36ebe6c4b83d18386aacff152ac2e6a9a0d2caec
Hubert Nguyen <hubertanguyen@gmail.com>
2015-06-04 20:29:16 -0500

    Ember location auto api, and root path in router

---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,7 @@
+App.Router.map(function() {
+  // autolocation API to get rid of hashes in url
+  location: 'auto',
+  // rootURL is Ember knows where to start parsing the url from.
+  rootURL: '/'
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 EmberHelloWorld::Application.routes.draw do
   root to: 'home#index'
+  # Catch all rails route to handle whatever arbitary routes we create in Ember
+  get "*path", to: 'home#index'
 end
