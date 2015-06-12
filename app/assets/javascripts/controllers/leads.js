b8718545c6d5a2db88fecf15c6e6afe350c2614c
Hubert Nguyen <hubertanguyen@gmail.com>
2015-06-11 22:41:50 -0500

    add search feature

--- app/assets/javascripts/controllers/leads.js
+++ app/assets/javascripts/controllers/leads.js
@@ -1,6 +1,16 @@
 // You can sort array of modles by specifiying sortProperties
 // sortProperties takes an array of strings, that are sorted with highest priority first
 App.LeadsController = Ember.ArrayController.extend({
-  sortProperties: ['firstName','lastName']
+  sortProperties: ['firstName','lastName'],
+  leads: function() {
+    return this.get('search') ? this.get('searchedLeads') : this;
+  }.property('search','searchedLeads'),
+
+  searchedLeads: function() {
+    var search = this.get('search').toLowerCase();
+    return this.filter(function(lead){
+      return lead.get('fullName').toLowerCase().indexOf(search) != -1;
+    });
+  }.property('search','this.@each.fullName')
 });
 
